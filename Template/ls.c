bc140e1823013c337f02f1c2d7c982110f90629e
Mike27290 <80801622+Mike27290@users.noreply.github.com>
2021-10-11 18:52:10 -0500

    Update ls.c

--- Template/ls.c
+++ Template/ls.c
@@ -13,7 +13,7 @@ void ls(char *path, bool recurse_flag) {
     		mydir = opendir(path);
     		while((directory = readdir(mydir)) != NULL)
     		{
-        		printf(" %s\n", directory->path);
+        		printf(" %s\n", directory->d_name);
     		}
     		closedir(mydir);
 	}
