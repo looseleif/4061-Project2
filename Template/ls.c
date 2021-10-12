d40a8bdc1f4586f65b6163657f1c0e77e6769e4c
Mike27290 <80801622+Mike27290@users.noreply.github.com>
2021-10-12 17:52:11 -0500

    Update ls.c

--- Template/ls.c
+++ Template/ls.c
@@ -9,23 +9,45 @@ void ls(char *path, bool recurse_flag) {
 	DIR *mydir; //DIR is a type that represents the directory
     	struct dirent *directory; //A variable that represents the directory as a whole
 
-	printf("before %s\n", path);
 
 	if(path != NULL) { 
-		printf("inside \n");
     		mydir = opendir(path); //This sets mydir equal to a pointer to the specified path
 
 		//readdir returns a pointer to the current position in the directory
     		while((directory = readdir(mydir)) != NULL) //readdir returns a null at the end of the directory
     		{	
-			printf("While loop \n");
-        		printf(" %s\n", directory->d_name); //d_name is an array that holds all the names of the directory
+        		printf(" %s ", directory->d_name); //d_name is an array that holds all the names of the directory
     		}
 		
-		printf("done \n");
+		printf("\n"); //Formatting
+		
     		closedir(mydir);
 	}
 	else if(path = NULL) {
+		int Max_Path_Size = 20; //Just a random number I picked
+		char curDir[Max_Path_Size]; //array that will represent our current directory
+		
+		if(getcwd(curDir, Max_Path_Size) == NULL) //This function returns NULL for an error
+		{
+			printf("CWD Error\n");
+		}
+		else //This code is copy and paste from above, may reformat to make more efficient
+		{
+			path = curDir; 
+
+			mydir = opendir(path); //This sets mydir equal to a pointer to the specified path
+
+			//readdir returns a pointer to the current position in the directory
+    			while((directory = readdir(mydir)) != NULL) //readdir returns a null at the end of the directory
+    			{	
+        			printf(" %s ", directory->d_name); //d_name is an array that holds all the names of the directory
+    			}
+		
+			printf("\n"); //Formatting
+		
+    			closedir(mydir);
+		}
+		
 		printf("path is NULL \n");
 	}
 }
