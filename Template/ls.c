af71944febd9c9aa6100b629caa6a6dbf64b4e9a
Mike27290 <80801622+Mike27290@users.noreply.github.com>
2021-10-11 19:16:05 -0500

    Update ls.c

--- Template/ls.c
+++ Template/ls.c
@@ -6,22 +6,27 @@
 #include <dirent.h>
 
 void ls(char *path, bool recurse_flag) {
-	DIR *mydir;
-    	struct dirent *directory;
+	DIR *mydir; //DIR is a type that represents the directory
+    	struct dirent *directory; //A variable that represents the directory as a whole
 
 	printf("before %s\n", path);
-	
-	if(path != NULL) {
+
+	if(path != NULL) { 
 		printf("inside \n");
-    		mydir = opendir(path);
-    		while((directory = readdir(mydir)) != NULL)
+    		mydir = opendir(path); //This sets mydir equal to a pointer to the specified path
+
+		//readdir returns a pointer to the current position in the directory
+    		while((directory = readdir(mydir)) != NULL) //readdir returns a null at the end of the directory
     		{	
 			printf("While loop \n");
-        		printf(" %s\n", directory->d_name);
+        		printf(" %s\n", directory->d_name); //d_name is an array that holds all the names of the directory
     		}
 		
 		printf("done \n");
     		closedir(mydir);
+	}
+	else if(path = NULL) {
+		printf("path is NULL \n");
 	}
 }
 
