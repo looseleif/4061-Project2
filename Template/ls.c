547a08b0995c2ad0a25ddf118fc2d7e9d711b1ef
Mike27290 <80801622+Mike27290@users.noreply.github.com>
2021-10-12 18:09:48 -0500

    Update ls.c

--- Template/ls.c
+++ Template/ls.c
@@ -9,6 +9,8 @@ void ls(char *path, bool recurse_flag) {
 	DIR *mydir; //DIR is a type that represents the directory
     	struct dirent *directory; //A variable that represents the directory as a whole
 
+	int excludePeriods = 0;
+
 
 	if(path != NULL) { 
     		mydir = opendir(path); //This sets mydir equal to a pointer to the specified path
@@ -16,7 +18,14 @@ void ls(char *path, bool recurse_flag) {
 		//readdir returns a pointer to the current position in the directory
     		while((directory = readdir(mydir)) != NULL) //readdir returns a null at the end of the directory
     		{	
-        		printf(" %s ", directory->d_name); //d_name is an array that holds all the names of the directory
+			if(excludePeriods < 2) 
+			{
+				excludePeriods++;
+			}
+			else
+			{
+        			printf(" %s ", directory->d_name); //d_name is an array that holds all the names of the directory
+			}
     		}
 		
 		printf("\n"); //Formatting
@@ -26,8 +35,6 @@ void ls(char *path, bool recurse_flag) {
 	else if(path == NULL) {
 		size_t Max_Path_Size = 100; //Just a random number I picked
 		char curDir[Max_Path_Size]; //array that will represent our current directory
-
-		printf("in the null\n");
 		
 		if(getcwd(curDir, Max_Path_Size) == NULL) //This function returns NULL for an error
 		{
@@ -35,7 +42,6 @@ void ls(char *path, bool recurse_flag) {
 		}
 		else //This code is copy and paste from above, may reformat to make more efficient
 		{
-			printf("In the else\n");
 			path = curDir; 
 
 			mydir = opendir(path); //This sets mydir equal to a pointer to the specified path
@@ -43,7 +49,14 @@ void ls(char *path, bool recurse_flag) {
 			//readdir returns a pointer to the current position in the directory
     			while((directory = readdir(mydir)) != NULL) //readdir returns a null at the end of the directory
     			{	
-        			printf(" %s ", directory->d_name); //d_name is an array that holds all the names of the directory
+				if(excludePeriods < 2) 
+				{
+					excludePeriods++;
+				}
+				else
+				{
+        				printf(" %s ", directory->d_name); //d_name is an array that holds all the names of the directory
+				}
     			}
 		
 			printf("\n"); //Formatting
@@ -51,7 +64,6 @@ void ls(char *path, bool recurse_flag) {
     			closedir(mydir);
 		}
 		
-		printf("path is NULL \n");
 	}
 }
 
