de3462dda8dc195bf1ca2c44d7df19a7c6d35407
Mike27290 <80801622+Mike27290@users.noreply.github.com>
2021-10-13 11:10:36 -0500

    Update ls.c

--- Template/ls.c
+++ Template/ls.c
@@ -107,7 +107,7 @@ void ls(char *path, bool recurse_flag) {
 
 			for (int i = 0; i < currentDirStorInt; i++)
 			{
-				printf("%s: \n", recursiveDirectoryStorage);
+				//printf("%s: \n", recursiveDirectoryStorage);
 				ls(recursiveDirectoryStorage, true);
 
 			}
