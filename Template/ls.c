8643209ae5289a0800a73ceb77656c13383be0d4
Mike27290 <80801622+Mike27290@users.noreply.github.com>
2021-10-13 11:13:22 -0500

    Update ls.c

--- Template/ls.c
+++ Template/ls.c
@@ -89,7 +89,7 @@ void ls(char *path, bool recurse_flag) {
 				{
 					if (directory->d_type == DT_DIR) //This checks if it is a folder
 					{
-						realpath(directory->d_name, recursiveDirectoryStorage[currentDirStorInt);
+						realpath(directory->d_name, recursiveDirectoryStorage[currentDirStorInt]);
 						//printf("HAHAH: %s\n", recursiveDirectoryStorage);
 						
 						currentDirStorInt++;
