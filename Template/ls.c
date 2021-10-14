bfac028b9052d1e76a83066fdcefdc5566fc937f
Mike27290 <80801622+Mike27290@users.noreply.github.com>
2021-10-14 16:08:33 -0500

    Update ls.c

--- Template/ls.c
+++ Template/ls.c
@@ -90,7 +90,7 @@ void ls(char *path, bool recurse_flag) {
 					if (directory->d_type == DT_DIR) //This checks if it is a folder
 					{
 						printf("HAHAH: %s\n", mydir);
-						sprintf(recursiveDirectoryStorage, "%s/%s", path,directory->d_name);
+						sprintf(recursiveDirectoryStorage, "%s/%s", mydir,directory->d_name);
 						//recursiveDirectoryStorage[currentDirStorInt] = mydir;
 						printf("HAHAH: %s\n", mydir);
 						
