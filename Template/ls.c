ca0e9ff7527e6b80e78bcbd8331c594adc654605
Mike27290 <80801622+Mike27290@users.noreply.github.com>
2021-10-15 16:14:04 -0500

    Update ls.c

--- Template/ls.c
+++ Template/ls.c
@@ -40,7 +40,7 @@ void ls(char *path, bool recurse_flag)
       	}
 	for(int i = 0; i < count; i++)
 	{
-		printf("in directory %s: \n", path1);
+		printf("in directory %s: \n", path);
 		ls(path1, true);
 	}
    closedir(dfd);
