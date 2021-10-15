3b49bf3e1badd6a49dfd2e3e3d03bdad2b0b999c
Mike27290 <80801622+Mike27290@users.noreply.github.com>
2021-10-15 15:51:39 -0500

    Update ls.c

--- Template/ls.c
+++ Template/ls.c
@@ -15,23 +15,26 @@ void ls(char *path, bool recurse_flag)
 	char path1[2000];
    	struct dirent *dp;
    	DIR *dfd;
- 
+ 	printf("1\n");
   	if ((dfd = opendir(path)) == NULL) 
 	{
       		fprintf(stderr, "lsrec: can't open %s\n", path);
       		return;
    	}
- 
+ 	printf("2\n");
    	while ((dp = readdir(dfd)) != NULL) 
 	{
-      		if (dp->d_type == DT_DIR)
+      		printf("3\n");
+		if (dp->d_type == DT_DIR)
 		{
-         		path[0] = '\0';
+         		printf("4\n");
+			path1[0] = '\0';
          		if (strcmp(dp->d_name, ".") == 0 || 
              		strcmp(dp->d_name, "..") == 0)
             			continue;
          		sprintf(path1, "%s/%s", path, dp->d_name);
          		ls(path1, true);
+			printf("5\n");
       		}
       		else
          		printf("%s/%s\n", path, dp->d_name);
