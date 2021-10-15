9b8eca457212eed08e2d5dc17f49b09d2f894fe0
Emir <sahbe001@umn.edu>
2021-10-15 01:06:46 -0500

    cd.c should be done!

--- Template/cd.c
+++ Template/cd.c
@@ -1,9 +1,57 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include <unistd.h>
+#include <errno.h>
 
 void cd(char* arg){
 	/*insert code here*/
+	
+	//gets the PC specific maximum path length for use in memory allocation
+	int PATH_SIZE = pathconf(".", _PC_PATH_MAX);
+
+	//error checking for pathconf
+	errno = 0;
+	if(PATH_SIZE < 0)
+	{
+		perror("ERROR: failed to retrieve maximum path size");
+	}
+
+	//allocated memory for and stores the current working directory file path
+	char * currentDirectory = (char *) malloc(sizeof(char) * (PATH_SIZE + 1));
+
+	//error checking for malloc
+	if(currentDirectory == NULL)
+	{
+		perror("ERROR: failed to allocate memory");
+	}
+
+	//gets current working directory & checks for error
+	if(getcwd(currentDirectory, PATH_SIZE) == NULL)
+	{
+		perror("ERROR: failed to get current working directory");
+	}
+
+	//printf("%s \n", arg);
+
+	//chdir & error checking
+	if(chdir(arg) < 0)
+	{
+		perror("ERROR: failed to change directory");
+	}
+
+	//gets current working directory & checks for error
+	if(getcwd(currentDirectory, PATH_SIZE) == NULL)
+	{
+		perror("ERROR: failed to get current working directory");
+	}
+
+	//prints current working directory and checks for errors
+	if(printf("%s\n", currentDirectory) < 0)
+	{
+		perror("ERROR: failed to print current working directory");
+	}
+	
+	free(currentDirectory);
 }
 
 int main(int argc, char** argv){
