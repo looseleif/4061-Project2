bbf28075e1274be26b4dd592cac3d9a6db74823a
Emir <sahbe001@umn.edu>
2021-10-17 15:26:24 -0500

    shell.c initialization (NOT TESTED)

--- Template/shell.c
+++ Template/shell.c
@@ -11,4 +11,37 @@
 
 int main(){
 	/*write code here*/
+
+	
+	char * currentDirectory;						//stores the cwd path
+	char SHELL_TAG[] = "[project-4061]";			//tag for printing out the cwd
+	int PATH_SIZE = pathconf(".", _PC_PATH_MAX);	//gets the PC specific maximum path length for use in memory allocation
+
+	//error checking for pathconf
+	errno = 0;
+	if(PATH_SIZE < 0)
+	{
+		perror("ERROR: failed to retrieve maximum path size");
+	}
+
+	//allocated memory for and stores the current working directory file path
+	currentDirectory = (char *) malloc(sizeof(char) * (PATH_SIZE + 1));
+
+	//error checking for malloc
+	if(currentDirectory == NULL)
+	{
+		perror("ERROR: failed to allocate memory");
+	}
+
+	//gets current working directory & checks for error
+	if(getcwd(currentDirectory, PATH_SIZE) == NULL)
+	{
+		perror("ERROR: failed to get current working directory");
+	}
+
+	//TODO: execute "clear" so the shell looks better
+
+	//prints out the tag and the cwd
+	printf("\n%s%s", SHELL_TAG, currentDirectory);
+
 }
