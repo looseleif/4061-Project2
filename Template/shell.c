517aea456e7088e0a175d1802121269b7be5de9d
Emir <sahbe001@umn.edu>
2021-10-17 23:05:49 -0500

    Initialization of shell v1

--- Template/shell.c
+++ Template/shell.c
@@ -11,7 +11,6 @@
 
 int main(){
 	/*write code here*/
-
 	
 	char * currentDirectory;						//stores the cwd path
 	char SHELL_TAG[] = "[project-4061]";			//tag for printing out the cwd
@@ -22,6 +21,7 @@ int main(){
 	if(PATH_SIZE < 0)
 	{
 		perror("ERROR: failed to retrieve maximum path size");
+		exit(0);
 	}
 
 	//allocated memory for and stores the current working directory file path
@@ -31,17 +31,47 @@ int main(){
 	if(currentDirectory == NULL)
 	{
 		perror("ERROR: failed to allocate memory");
+		free(currentDirectory);
+		exit(0);
 	}
 
 	//gets current working directory & checks for error
 	if(getcwd(currentDirectory, PATH_SIZE) == NULL)
 	{
 		perror("ERROR: failed to get current working directory");
+		free(currentDirectory);
+		exit(0);
 	}
 
-	//TODO: execute "clear" so the shell looks better
+	//creating process to execute clear
+	pid_t p1 = fork();
+	if(p1 < 0) //error
+	{
+		perror("ERROR: failed to fork p1");
+		free(currentDirectory);
+		exit(0);
+	}
+	else if (p1 == 0) //child
+	{
+		free(currentDirectory);
+		execl("/bin/clear", "/bin/clear", (char *) NULL);
 
-	//prints out the tag and the cwd
-	printf("\n%s%s", SHELL_TAG, currentDirectory);
+		//error checking execl
+		perror("ERROR: failed execute clear");
+		exit(0);
+	}
+	else  //parent
+	{
+		//wait for p1, error checking
+		if(waitpid(p1, NULL, 0) < 0) {perror("ERROR: failed to wait for child p1"); free(currentDirectory); exit(0);}
 
+		// Initialization should end here. Now, we must print the cwd and wait for input. While loop might be a good idea
+
+		//prints out the tag and the cwd
+		printf("\n%s%s $ \n", SHELL_TAG, currentDirectory);
+
+		//TODO: free memory in exit()
+		free(currentDirectory);
+		return 0;
+	}
 }
