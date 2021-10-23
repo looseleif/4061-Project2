34a5d7ce3a2556f9b44b4cb0009530333c4d2a8d
Emir <sahbe001@umn.edu>
2021-10-23 00:55:22 -0500

    BROKEN: but almost there! (this will infinitely recurse)

--- Template/shell.c
+++ Template/shell.c
@@ -9,31 +9,62 @@
 #include <fcntl.h>
 #include "util.h"
 
-int thePipeProcess(char* commands[50], int Index, int cmdCounter) {
+int thePipeProcess(char* commands[50], int Index, int cmdCounter, char * TEMPLATE_DIR) {
 
-	printf("PipePro\n");
+	//printf("PipePro\n");
 
 	char** inPipe = (char**)malloc(sizeof(char**) * Index);;
 	char** outPipe = (char**)malloc(sizeof(char**) * (cmdCounter - Index));; // check later
+	char*** pipeSelect = (char***)malloc(sizeof(char***));
+	int start, end;
+	int ifRedirected = 0;
+	int retVal = 0;
+	int file_desc;
+	int terminal = dup(STDOUT_FILENO);
+	int inputFD = dup(STDIN_FILENO);
 
 	int i;
 
+	//fill inPipe
 	for (i = 0; i < Index; i++) {
 
 		inPipe[i] = (char*)malloc(100);
+		//printf("cmds: %s\n", commands[i]);
 		strcpy(inPipe[i], commands[i]); 
-		//printf("pipe: %s\n", inPipe[i]);
+		//printf("inPipe: %s\n", inPipe[i]);
 
 	}
 
-	//printf("-----\n");
-
-	for (i = Index + 1; i < cmdCounter; i++) {
-
+	//fill outPipe
+	int j;
+	for (i = 0, j = Index + 1; j < cmdCounter; j++, i++)
+	{
 		outPipe[i] = (char*)malloc(100);
-		strcpy(outPipe[i], commands[i]);
-		//printf("pipe: %s\n", outPipe[i]);
+		//printf("cmds: %s\n", commands[j]);
+		strcpy(outPipe[i], commands[j]);
+		//printf("outPipe: %s\n", outPipe[i]);
 
+		if(strcmp(outPipe[i], ">") == 0) //if no append
+		{
+			fclose(fopen(outPipe[i+1], "w")); //clears the output file
+			file_desc = open(outPipe[i+1], O_WRONLY | O_CREAT); //opens the file
+			if(file_desc < 0) //error checking
+			{
+ 				printf("Error opening the file\n");
+			}
+			dup2(file_desc, 1); //changes the output to the file
+			ifRedirected = 1;
+		}
+		else if (strcmp(outPipe[i], ">>") == 0) //if append
+		{				
+			file_desc = open(outPipe[i+1], O_WRONLY | O_APPEND | O_CREAT); //opens the file
+			if(file_desc < 0) //error checking
+			{
+ 				printf("Error opening the file\n");
+			}
+			dup2(file_desc, STDOUT_FILENO); //changes the output to the file
+			ifRedirected = 1;
+		}
 	}
 
 	// DONE WITH CMD SEGMENTATION
@@ -60,49 +91,61 @@ int thePipeProcess(char* commands[50], int Index, int cmdCounter) {
 	}
 	else if (pipePID > 0) { // parent
 
-		wait(NULL);
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
 
-
+		pipeSelect[0] = outPipe;
+		start = Index + 1;
+		end = cmdCounter;
 
 	}
-	else {
+	else { //child INPUT
 
 		dup2(fd[1], STDOUT_FILENO);
 		close(fd[0]);
 		close(fd[1]);
 
+		pipeSelect[0] = inPipe;
+		start = 0;
+		end = Index;
+
 	}
 
 	// OUTSIDE OF FORK CONDITIONS
 
-	/*if (strcmp(commandSplit[0], "cd") == 0)
+	pid_t c2PID;
+	
+	if (strcmp(pipeSelect[0][0], "cd") == 0)
 	{
-		if (counter != 2)
+		if ((end - start) != 2)
 		{
 			printf("Command error\n");
 
 		}
-		else if (chdir(commandSplit[1]) < 0)
+		else if (chdir(pipeSelect[0][1]) < 0)
 		{
 			perror("[4061-shell]: ");
 		}
 	}
-	else if ((strcmp(commandSplit[0], "ls") == 0) || (strcmp(commandSplit[0], "wc") == 0))
+	else if ((strcmp(pipeSelect[0][0], "ls") == 0) || (strcmp(pipeSelect[0][0], "wc") == 0))
 	{
-		pid_t customPID = fork();
-
-		if (customPID < 0) //error
+		if(pipePID > 0) //SUPER PARENT
 		{
+			c2PID = fork();
+			if (c2PID < 0) //error
+			{
 			printf("Command error\n");
+			}	
 		}
-		else if (customPID == 0) //child
+		if (pipePID == 0 || c2PID == 0) //child
 		{
 			char* ABS_PATH_BUF = (char*)malloc(1000);
-			sprintf(ABS_PATH_BUF, "%s/%s", TEMPLATE_DIR, commandSplit[0]);
-			commandSplit[0] = ABS_PATH_BUF;
+			sprintf(ABS_PATH_BUF, "%s/%s", TEMPLATE_DIR, pipeSelect[0][0]);
+			pipeSelect[0][0] = ABS_PATH_BUF;
 
 			int j = 0;
-			while (j < counter && (strcmp(commandSplit[j], ">") != 0) && (strcmp(commandSplit[j], ">>") != 0))
+			while (j < (end - start) && (strcmp(pipeSelect[0][j], ">") != 0) && (strcmp(pipeSelect[0][j], ">>") != 0))
 			{
 				j++;
 			}
@@ -113,42 +156,48 @@ int thePipeProcess(char* commands[50], int Index, int cmdCounter) {
 			for (i = 0; i < j; i++)
 			{
 				args[i] = (char*)malloc(100);
-				strcpy(args[i], commandSplit[i]);
+				printf("%d: %s\n", i, pipeSelect[0][i]);
+				strcpy(args[i], pipeSelect[0][i]);
 			}
 			args[j] = (char*)NULL;
 
 			execv(args[0], args);
 			printf("FAIL\n");
-			free(currentDirectory);
-			free(buf);
-			free(TEMPLATE_DIR);
-			return 0;
+			free(ABS_PATH_BUF);
+			free(args);
+			retVal = -1;
 		}
 		else //parent
 		{
 			//wait for lsPID, error checking
-			if (waitpid(customPID, NULL, 0) < 0)
+			if (waitpid(pipePID, NULL, 0) < 0)
 			{
 				printf("Command error\n");
-				free(currentDirectory);
-				free(buf);
-				free(TEMPLATE_DIR);
+				exit(0);
+			}
+			//wait for lsPID, error checking
+			if (waitpid(c2PID, NULL, 0) < 0)
+			{
+				printf("Command error\n");
 				exit(0);
 			}
 		}
 	}
 	else
 	{
-		pid_t miscPID = fork();
 
-		if (miscPID < 0) //error
+		if(pipePID > 0) //SUPER PARENT
 		{
+			c2PID = fork();
+			if (c2PID < 0) //error
+			{
 			printf("Command error\n");
+			}	
 		}
-		else if (miscPID == 0) //child
+		if (c2PID == 0 || pipePID == 0) //child
 		{
 			int j = 0;
-			while (j < counter && (strcmp(commandSplit[j], ">") != 0) && (strcmp(commandSplit[j], ">>") != 0))
+			while (j < (end - start) && (strcmp(pipeSelect[0][j], ">") != 0) && (strcmp(pipeSelect[0][j], ">>") != 0))
 			{
 				j++;
 			}
@@ -159,34 +208,47 @@ int thePipeProcess(char* commands[50], int Index, int cmdCounter) {
 			for (i = 0; i < j; i++)
 			{
 				args[i] = (char*)malloc(100);
-				strcpy(args[i], commandSplit[i]);
+				strcpy(args[i], pipeSelect[0][i]);
 			}
 			args[j] = (char*)NULL;
 
 			execvp(args[0], args);
 			printf("FAIL\n");
-			free(currentDirectory);
-			free(buf);
-			free(TEMPLATE_DIR);
-			return 0;
+			free(args);
+			retVal = -1;
 			//execvp(commandSplit[0],commandSplit);
 		}
 		else //parent
 		{
-			//wait for miscPID, error checking
-			if (waitpid(miscPID, NULL, 0) < 0)
+			if (waitpid(pipePID, NULL, 0) < 0)
 			{
 				printf("Command error\n");
-				free(currentDirectory);
-				free(buf);
-				free(TEMPLATE_DIR);
+				exit(0);
+			}
+			//wait for miscPID, error checking
+			if (waitpid(c2PID, NULL, 0) < 0)
+			{
+				printf("Command error\n");
 				exit(0);
 			}
 		}
-	} */
+	}
 	
-	return 0;
+	free(inPipe);
+	free(outPipe);
+	free(pipeSelect);
+	close(fd[0]);
+	close(fd[1]);
 
+	if(ifRedirected == 1)
+	{	
+		close(file_desc);
+		dup2(terminal,1);
+		dup2(inputFD,0);
+		ifRedirected = 0;
+	}
+
+	return retVal;
 }
 
 
@@ -198,7 +260,7 @@ int main(){
 	int MAX_COMMAND_SIZE = 1000;
 	int PATH_SIZE = pathconf(".", _PC_PATH_MAX);	//gets the PC specific maximum path length for use in memory allocation
 	int redirectionType = 0;
-	int pipe = 0;
+	int isPiping = 0;
 	int file_desc;
 	int terminal = dup(STDOUT_FILENO);
 	int ifRedirected = 0;
@@ -319,9 +381,8 @@ int main(){
    			}
 		
 			int pipeIndex;
-			int isPiping = 0;
 	
-		for(int i = 0; i < counter; i++) //checking for file redirection
+		for(int i = 0; i < counter && isPiping == 0; i++) //checking for file redirection
 		{
 			
 			if (strcmp(commandSplit[i], "|") == 0) {
@@ -329,7 +390,7 @@ int main(){
 				isPiping = 1;
 				pipeIndex = i;
 
-				if (thePipeProcess(commandSplit, pipeIndex, counter) < 0) {
+				if (thePipeProcess(commandSplit, pipeIndex, counter, TEMPLATE_DIR) < 0) {
 
 					printf("Command Error\n");
 					free(currentDirectory);
@@ -341,11 +402,10 @@ int main(){
 
 				printf("ended Pipe\n");
 
-				return 0; // FOR DEV PURPOSES
+				//return 0; // FOR DEV PURPOSES
 
 			}
-			
-			if(strcmp(commandSplit[i], ">") == 0) //if no append
+			else if(strcmp(commandSplit[i], ">") == 0) //if no append
 			{
 				fclose(fopen(commandSplit[i+1], "w")); //clears the output file
 				file_desc = open(commandSplit[i+1], O_WRONLY | O_CREAT); //opens the file
@@ -368,7 +428,8 @@ int main(){
 				ifRedirected = 1;
 			}
 		}
-		
+		if (isPiping == 0) 
+		{
 		if(strcmp(commandSplit[0], "cd") == 0)
 		{
 			if(counter != 2)
@@ -484,6 +545,11 @@ int main(){
 			close(file_desc);
 			dup2(terminal,1);
 			ifRedirected = 0;
+		}
+		
+		}
+		else {
+			isPiping = 0;
 		}
 
 		//gets current working directory & checks for error
