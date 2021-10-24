ce3593f2889799399e954ac1900c59835840d661
Mike27290 <skeanasheka@gmail.com>
2021-10-23 22:20:25 -0500

    COMMENTS ARE DONE

--- Template/shell.c
+++ Template/shell.c
@@ -23,26 +23,22 @@ int thePipeProcess(char* commands[50], int Index, int cmdCounter, char * TEMPLAT
 
 	int i;
 
-	//fill inPipe
-	for (i = 0; i < Index; i++) {
-
+	//fill inPipe with the command before the pipe
+	for (i = 0; i < Index; i++) 
+	{
 		inPipe[i] = (char*)malloc(100);
-		//printf("cmds: %s\n", commands[i]);
 		strcpy(inPipe[i], commands[i]); 
-		//printf("inPipe: %s\n", inPipe[i]);
 
 	}
 
-	//fill outPipe
+	//fill outPipe with the command after the pipe
 	int j;
 	for (i = 0, j = Index + 1; j < cmdCounter; j++, i++)
 	{
 		outPipe[i] = (char*)malloc(100);
-		//printf("cmds: %s\n", commands[j]);
 		strcpy(outPipe[i], commands[j]);
-		//printf("outPipe: %s\n", outPipe[i]);
-
-
+		
+		//Checking for file redirection, copy of main
 		if(strcmp(outPipe[i], ">") == 0) //if no append
 		{
 			fclose(fopen(commands[j+1], "w")); //clears the output file
@@ -51,13 +47,7 @@ int thePipeProcess(char* commands[50], int Index, int cmdCounter, char * TEMPLAT
 			{
  				printf("Error opening the file\n");
 			}
-
-			
-
-			 //changes the output to the file
-
-			// RETURN DUP2
-
+			//for clearing purposes
 			ifRedirected = 1;
 		}
 		else if (strcmp(outPipe[i], ">>") == 0) //if append
@@ -67,18 +57,15 @@ int thePipeProcess(char* commands[50], int Index, int cmdCounter, char * TEMPLAT
 			{
  				printf("Error opening the file\n");
 			}
-			 //changes the output to the file
-
-			// RETURN DUP2
-
+			//for clearing purposes
 			ifRedirected = 1;
 		}
 	}
 
 	// DONE WITH CMD SEGMENTATION
-
 	int fd[2];
 
+	//Error checking
 	if (pipe(fd) < 0) {
 
 		printf("Command Error\n");
@@ -89,6 +76,7 @@ int thePipeProcess(char* commands[50], int Index, int cmdCounter, char * TEMPLAT
 
 	pid_t pipePID = fork();
 
+	//Error checking
 	if (pipePID < 0) {
 
 		perror("Command Error");
@@ -97,26 +85,28 @@ int thePipeProcess(char* commands[50], int Index, int cmdCounter, char * TEMPLAT
 		return -1;
 
 	}
-	else if (pipePID > 0) { // parent
-
+	//if it is the parent, it will set the pipeSelect string pointer equal to the outPipe
+	else if (pipePID > 0) 
+	{
 		wait(NULL);
-
-		//close(fd[0]);
-		//close(fd[1]);
 
 		pipeSelect[0] = outPipe;
 		start = Index + 1;
 		end = cmdCounter;
 
 	}
-	else { //child INPUT
 
-		if (dup2(fd[1], STDOUT_FILENO) < 0) {
+	//child input
+	else 
+	{
+
+		if (dup2(fd[1], STDOUT_FILENO) < 0) 
+		{
 
 			fprintf(stderr, "ERROR: Failed dup2... fd:\n");
-
 		}
-
+		
+		//Sets the pipeSelect string pointer equal to the inPipe
 		pipeSelect[0] = inPipe;
 		start = 0;
 		end = Index;
@@ -127,65 +117,78 @@ int thePipeProcess(char* commands[50], int Index, int cmdCounter, char * TEMPLAT
 
 	pid_t c2PID;
 	
+	//Checking for cd 
 	if (strcmp(pipeSelect[0][0], "cd") == 0)
 	{
+		//if there are more than two arguments
 		if ((end - start) != 2)
 		{
 			printf("Command error1\n");
 
 		}
-		else if (chdir(pipeSelect[0][1]) < 0)
+
+		//Error checking
+		else if (chdir(pipeSelect[0][1]) < 0) 
 		{
 			perror("[4061-shell]: ");
 		}
 	}
+	
+	//Checking for cd or ls and executing
 	else if ((strcmp(pipeSelect[0][0], "ls") == 0) || (strcmp(pipeSelect[0][0], "wc") == 0))
 	{
+		//Parent of two children
 		if(pipePID > 0) //SUPER PARENT
 		{
 			c2PID = fork();
-			if (c2PID < 0) //error
+
+			//Error checking
+			if (c2PID < 0) 
 			{
-			printf("Command error2\n");
+				printf("Command error2\n");
 			}	
 		}
-		if (pipePID == 0 || c2PID == 0) //child
+		
+		//Runs only for child processes
+		if (pipePID == 0 || c2PID == 0)
 		{
+			//Allocates memory for path and puts it in pipeSelect
 			char* ABS_PATH_BUF = (char*)malloc(1000);
 			sprintf(ABS_PATH_BUF, "%s/%s", TEMPLATE_DIR, pipeSelect[0][0]);
 			pipeSelect[0][0] = ABS_PATH_BUF;
 
-			if (c2PID == 0) {
+			//Runs only for the second child
+			if (c2PID == 0) 
+			{
+				if (dup2(fd[0], STDIN_FILENO) < 0) 
+				{
+					fprintf(stderr, "ERROR: Failed dup2 for child c2 \n");
+				}
+			}
+			
+			//TESTING
+			if (pipePID == 0) 
+			{
+				close(fd[0]);
+				close(fd[1]);
+			}
+			
+			
+			if (c2PID==0 && ifRedirected) 
+			{
+				//Error checking
+				if (dup2(file_desc, STDOUT_FILENO) < 0) 
+				{
 
-				if (dup2(fd[0], STDIN_FILENO) < 0) {
-
-					fprintf(stderr, "ERROR: Failed du123123123p2... fd: \n");
+					fprintf(stderr, "ERROR: Failed dup2 for file redirection for c2 fd:\n");
 
 				}
 			}
 
-			if (pipePID == 0) {
+			close(fd[0]);
+			close(fd[1]);
 
-				close(fd[0]);
-				close(fd[1]);
-
-			}
-				
-			if (c2PID==0 && ifRedirected) {
-
-					if (dup2(file_desc, STDOUT_FILENO) < 0) {
-
-						fprintf(stderr, "ERROR: Failed dup1232... fd:\n");
-
-					}
-
-			}
-
-				close(fd[0]);
-				close(fd[1]);
-
-			
-
+			//Finds the position of the file redirection char
 			int j = 0;
 			while (j < (end - start) && (strcmp(pipeSelect[0][j], ">") != 0) && (strcmp(pipeSelect[0][j], ">>") != 0))
 			{
@@ -194,33 +197,33 @@ int thePipeProcess(char* commands[50], int Index, int cmdCounter, char * TEMPLAT
 
 			char** args = (char**)malloc(sizeof(char**) * j + 1);
 
+			//Creates the argument array and sets it equal to pipeSelect
+			//pipeSelect represents either the inPipe or outPipe
 			int i;
 			for (i = 0; i < j; i++)
 			{
 				args[i] = (char*)malloc(100);
-				//printf("%d: %s\n", i, pipeSelect[0][i]);
 				strcpy(args[i], pipeSelect[0][i]);
 			}
+
+			//Sets the last character in the argument array to NULL
 			args[j] = (char*)NULL;
 
 			execv(args[0], args);
+
+			//Error checking
 			printf("FAIL\n");
 			free(ABS_PATH_BUF);
 			free(args);
 			retVal = -1;
 		}
-		else //parent
-		{
 
+		//parent
+		else
+		{
 			close(fd[0]);
 			close(fd[1]);
 
-			//wait for lsPID, error checking
-			//if (waitpid(pipePID, NULL, 0) < 0)
-			//{
-			//	printf("Command error2\n");
-			//	exit(0);
-			//}
 			//wait for lsPID, error checking
 			if (waitpid(c2PID, NULL, 0) < 0)
 			{
@@ -229,6 +232,8 @@ int thePipeProcess(char* commands[50], int Index, int cmdCounter, char * TEMPLAT
 			}
 		}
 	}
+
+	//If the input command is not wc, ls, or cd
 	else
 	{
 
@@ -236,33 +241,38 @@ int thePipeProcess(char* commands[50], int Index, int cmdCounter, char * TEMPLAT
 		{
 			c2PID = fork();
 
-			if (c2PID < 0) //error
+			//Error checking
+			if (c2PID < 0)
 			{
-			printf("Command error\n");
+				printf("C2 forking error\n");
 			}	
 		}
-		if (c2PID == 0 || pipePID == 0) //child
+
+		//Runs only for child processes
+		if (c2PID == 0 || pipePID == 0)
 		{
+			//Finds the position of the file redirection char 
 			int j = 0;
 			while (j < (end - start) && (strcmp(pipeSelect[0][j], ">") != 0) && (strcmp(pipeSelect[0][j], ">>") != 0))
 			{
 				j++;
 			}
 
-			if (c2PID == 0) {
+			//Runs only for the second child
+			if (c2PID == 0) 
+			{
+				if (dup2(fd[0], STDIN_FILENO) < 0) 
+				{
 
-				if (dup2(fd[0], STDIN_FILENO) < 0) {
-
-					fprintf(stderr, "ERROR: Failed dup2... fd: \n");
+					fprintf(stderr, "ERROR: Failed dup2 for child 2 (misc)\n");
 
 				}
 
-				if (ifRedirected) {
-
-					if (dup2(file_desc, STDOUT_FILENO) < 0) {
-
-						fprintf(stderr, "ERROR: Failed dup2... fd:\n");
-
+				if (ifRedirected) 
+				{
+					if (dup2(file_desc, STDOUT_FILENO) < 0) 
+					{
+						fprintf(stderr, "ERROR: Failed dup2 for child 2 (misc and file redirection)\n");
 					}
 
 				}
@@ -274,31 +284,31 @@ int thePipeProcess(char* commands[50], int Index, int cmdCounter, char * TEMPLAT
 
 			char** args = (char**)malloc(sizeof(char**) * j + 1);
 
+			//Creates the argument array and sets it equal to pipeSelect
 			int i;
 			for (i = 0; i < j; i++)
 			{
 				args[i] = (char*)malloc(100);
 				strcpy(args[i], pipeSelect[0][i]);
 			}
+
+			//Sets the last character in the argument array to NULL 
 			args[j] = (char*)NULL;
 			
 			execvp(args[0], args);
+
+			//Error checking
 			printf("FAIL\n");
 			free(args);
 			retVal = -1;
-			//execvp(commandSplit[0],commandSplit);
 		}
-		else //parent
+		
+		//parent
+		else
 		{
-			
 			close(fd[0]);
 			close(fd[1]);
-
-			if (waitpid(pipePID, NULL, 0) < 0)
-			{
-				printf("Command error\n");
-				exit(0);
-			}
+			
 			//wait for miscPID, error checking
 			if (waitpid(c2PID, NULL, 0) < 0)
 			{
@@ -307,21 +317,20 @@ int thePipeProcess(char* commands[50], int Index, int cmdCounter, char * TEMPLAT
 			}
 		}
 	}
-
-	if (dup2(terminal, 1) < 0) {
-
+	
+	//Error checking
+	if (dup2(terminal, 1) < 0) 
+	{
 		fprintf(stderr, "ERROR: Failed dup2... fd:\n");
-
 	}
 
-
-	if (dup2(inputFD, 0) < 0) {
-
+	//Error checking
+	if (dup2(inputFD, 0) < 0) 
+	{
 		fprintf(stderr, "ERROR: Failed dup2... fd: \n");
-
 	}
 	
-
+	//If the program did file redirection, close file_desc
 	if(ifRedirected == 1)
 	{	
 		
@@ -329,14 +338,13 @@ int thePipeProcess(char* commands[50], int Index, int cmdCounter, char * TEMPLAT
 		ifRedirected = 0;
 
 	}
-
+	
+	//Free everything
 	free(inPipe);
 	free(outPipe);
 	free(pipeSelect);
 	close(fd[0]);
 	close(fd[1]);
-
-	//printf("WE GO HERE\n");
 	
 	return retVal;
 }
@@ -344,9 +352,9 @@ int thePipeProcess(char* commands[50], int Index, int cmdCounter, char * TEMPLAT
 
 int main(){
 	
-	char * currentDirectory;						//stores the cwd path
+	char * currentDirectory;			//stores the cwd path
 	char * TEMPLATE_DIR;
-	char SHELL_TAG[] = "[project-4061]";			//tag for printing out the cwd
+	char SHELL_TAG[] = "[project-4061]";		//tag for printing out the cwd
 	int MAX_COMMAND_SIZE = 1000;
 	int PATH_SIZE = pathconf(".", _PC_PATH_MAX);	//gets the PC specific maximum path length for use in memory allocation
 	int redirectionType = 0;
@@ -457,188 +465,217 @@ int main(){
 		while(strcmp(buf, "exit\n") != 0)
 		{
 			char * commandSplit[50];
-    		int counter = 0;
+    			int counter = 0;
     
-    		// Gets the first argument of the command (the command itself)
-    		commandSplit[0] = strtok(buf, " \n\r");
+    			// Gets the first argument of the command (the command itself)
+    			commandSplit[0] = strtok(buf, " \n\r");
 
-    		// Get the rest of the arguments
-    		while (commandSplit[counter] != NULL)
-    		{
-			//printf("%d: %s\n",counter, commandSplit[counter]);
-        		counter = counter + 1;
-        		commandSplit[counter] = strtok(NULL, " \n\r");
+    			// Get the rest of the arguments
+    			while (commandSplit[counter] != NULL)
+    			{
+        			counter = counter + 1;
+        			commandSplit[counter] = strtok(NULL, " \n\r");
    			}
 		
 			int pipeIndex;
-	
-		for(int i = 0; i < counter && isPiping == 0; i++) //checking for file redirection
-		{
 			
-			if (strcmp(commandSplit[i], "|") == 0) {
+			//checking for file redirection
+			for(int i = 0; i < counter && isPiping == 0; i++) 
+			{
+				//Checking for pipe
+				if (strcmp(commandSplit[i], "|") == 0) 
+				{
+					isPiping = 1;
+					pipeIndex = i;
 
-				isPiping = 1;
-				pipeIndex = i;
+					//Error checking
+					if (thePipeProcess(commandSplit, pipeIndex, counter, TEMPLATE_DIR) < 0) 
+					{
+						printf("Command Error\n");
+						free(currentDirectory);
+						free(buf);
+						free(TEMPLATE_DIR);
+						return 0;
+					};
 
-				if (thePipeProcess(commandSplit, pipeIndex, counter, TEMPLATE_DIR) < 0) {
 
-					printf("Command Error\n");
+				}
+				
+				//Checking for file redirection
+				else if(strcmp(commandSplit[i], ">") == 0) //if no append
+				{
+					//clears the output file
+					fclose(fopen(commandSplit[i+1], "w"));
+					
+					//opens the file
+					file_desc = open(commandSplit[i+1], O_WRONLY | O_CREAT); 
+					if(file_desc < 0) //error checking
+					{
+       	 					printf("Error opening the file\n");
+					}
+					dup2(file_desc, 1); //changes the output to the file
+					ifRedirected = 1;
+				}
+
+				else if (strcmp(commandSplit[i], ">>") == 0) //if append
+				{
+					//opens the file
+					file_desc = open(commandSplit[i+1], O_WRONLY | O_APPEND | O_CREAT);
+					if(file_desc < 0) //error checking
+					{
+       	 					printf("Error opening the file\n");
+					}
+					dup2(file_desc, STDOUT_FILENO); //changes the output to the file
+					ifRedirected = 1;
+				}
+		}
+		
+		//If we didn't pipe, check for commands and run them
+		if (isPiping == 0) 
+		{
+			//Checking for cd and running it
+			if(strcmp(commandSplit[0], "cd") == 0)
+			{
+				if(counter != 2)
+				{
+					printf("Command error\n");
+
+				}
+				else if(chdir(commandSplit[1]) < 0)
+				{
+					perror("[4061-shell]: ");
+				}
+			}
+			
+			//Checking for ls and wc and executing them
+			else if((strcmp(commandSplit[0], "ls") == 0) || (strcmp(commandSplit[0], "wc") == 0))
+			{
+				pid_t customPID = fork();
+
+				if (customPID < 0) //error
+				{
+					printf("Command error\n");
+				}
+				else if (customPID == 0) //child
+				{
+					//Making the absolute path to the command and putting into commandsplit
+					char * ABS_PATH_BUF = (char*) malloc(1000);
+					sprintf(ABS_PATH_BUF, "%s/%s", TEMPLATE_DIR, commandSplit[0]);
+					commandSplit[0] = ABS_PATH_BUF;
+
+					//Finding the index value of the file redirection
+					int j = 0;
+					while(j < counter && (strcmp(commandSplit[j], ">") != 0) && (strcmp(commandSplit[j], ">>") != 0))
+					{
+						j++;
+					}
+
+					char ** args = (char **) malloc(sizeof(char**) * j + 1);
+
+					//Creating the argument array
+					int i;
+					for(i = 0; i < j; i++)
+					{
+						args[i] = (char *) malloc(100);
+						strcpy(args[i], commandSplit[i]);
+					}
+					args[j] = (char *) NULL;
+				
+					execv(args[0],args);
+
+					//Error checking
+					printf("FAIL\n");
 					free(currentDirectory);
 					free(buf);
 					free(TEMPLATE_DIR);
 					return 0;
-
-				};
-
-				printf("ended Pipe\n");
-
-				//return 0; // FOR DEV PURPOSES
-
-			}
-			else if(strcmp(commandSplit[i], ">") == 0) //if no append
-			{
-				fclose(fopen(commandSplit[i+1], "w")); //clears the output file
-				file_desc = open(commandSplit[i+1], O_WRONLY | O_CREAT); //opens the file
-				if(file_desc < 0) //error checking
-				{
-       	 				printf("Error opening the file\n");
 				}
-				dup2(file_desc, 1); //changes the output to the file
-				ifRedirected = 1;
+
+				//parent
+				else 
+				{
+					//wait for lsPID, error checking
+					if(waitpid(customPID, NULL, 0) < 0) 
+					{
+						printf("Command error\n"); 
+						free(currentDirectory);
+						free(buf);
+						free(TEMPLATE_DIR);
+						exit(0);
+					}
+				}
 			}
-			else if (strcmp(commandSplit[i], ">>") == 0) //if append
+			else
 			{
+				pid_t miscPID = fork();
 				
-				file_desc = open(commandSplit[i+1], O_WRONLY | O_APPEND | O_CREAT); //opens the file
-				if(file_desc < 0) //error checking
+				//Error checking
+				if (miscPID < 0) 
 				{
-       	 				printf("Error opening the file\n");
+					printf("Command error\n");
 				}
-				dup2(file_desc, STDOUT_FILENO); //changes the output to the file
-				ifRedirected = 1;
-			}
-		}
-		if (isPiping == 0) 
-		{
-		if(strcmp(commandSplit[0], "cd") == 0)
-		{
-			if(counter != 2)
-			{
-				printf("Command error\n");
-
-			}
-			else if(chdir(commandSplit[1]) < 0)
-			{
-				perror("[4061-shell]: ");
-			}
-		}
-		else if((strcmp(commandSplit[0], "ls") == 0) || (strcmp(commandSplit[0], "wc") == 0))
-		{
-			pid_t customPID = fork();
-
-			if (customPID < 0) //error
-			{
-				printf("Command error\n");
-			}
-			else if (customPID == 0) //child
-			{
-				char * ABS_PATH_BUF = (char*) malloc(1000);
-				sprintf(ABS_PATH_BUF, "%s/%s", TEMPLATE_DIR, commandSplit[0]);
-				commandSplit[0] = ABS_PATH_BUF;
-
-				int j = 0;
-				while(j < counter && (strcmp(commandSplit[j], ">") != 0) && (strcmp(commandSplit[j], ">>") != 0))
-				{
-					j++;
-				}
-
-				char ** args = (char **) malloc(sizeof(char**) * j + 1);
-
-				int i;
-				for(i = 0; i < j; i++)
-				{
-					args[i] = (char *) malloc(100);
-					strcpy(args[i], commandSplit[i]);
-				}
-				args[j] = (char *) NULL;
 				
-				execv(args[0],args);
-				printf("FAIL\n");
-				free(currentDirectory);
-				free(buf);
-				free(TEMPLATE_DIR);
-				return 0;
-			}
-			else //parent
-			{
-				//wait for lsPID, error checking
-				if(waitpid(customPID, NULL, 0) < 0) 
+				//Child
+				else if (miscPID == 0)
 				{
-					printf("Command error\n"); 
+					//Finding the index value of the file redirection char
+					int j = 0;
+					while(j < counter && (strcmp(commandSplit[j], ">") != 0) && (strcmp(commandSplit[j], ">>") != 0))
+					{
+						j++;
+					}
+
+					char ** args = (char **) malloc(sizeof(char**) * j + 1);
+
+					//Creating the argument array
+					int i;
+					for(i = 0; i < j; i++)
+					{
+						args[i] = (char *) malloc(100);
+						strcpy(args[i], commandSplit[i]);
+					}
+					
+					//Setting the final char in the argument array to NULL
+					args[j] = (char *) NULL;
+			
+					execvp(args[0],args);
+
+					//Error checking
+					printf("FAIL\n");
 					free(currentDirectory);
 					free(buf);
 					free(TEMPLATE_DIR);
-					exit(0);
+					return 0;
+				}
+
+				//parent
+				else
+				{
+					//wait for miscPID, error checking
+					if(waitpid(miscPID, NULL, 0) < 0)
+					{
+						printf("Command error\n"); 
+						free(currentDirectory); 
+						free(buf); 
+						free(TEMPLATE_DIR);
+						exit(0);
+					}
 				}
 			}
-		}
-		else
-		{
-			pid_t miscPID = fork();
-
-			if (miscPID < 0) //error
-			{
-				printf("Command error\n");
-			}
-			else if (miscPID == 0) //child
-			{
-				int j = 0;
-				while(j < counter && (strcmp(commandSplit[j], ">") != 0) && (strcmp(commandSplit[j], ">>") != 0))
-				{
-					j++;
-				}
-
-				char ** args = (char **) malloc(sizeof(char**) * j + 1);
-
-				int i;
-				for(i = 0; i < j; i++)
-				{
-					args[i] = (char *) malloc(100);
-					strcpy(args[i], commandSplit[i]);
-				}
-				args[j] = (char *) NULL;
 			
-				execvp(args[0],args);
-				printf("FAIL\n");
-				free(currentDirectory);
-				free(buf);
-				free(TEMPLATE_DIR);
-				return 0;
-				//execvp(commandSplit[0],commandSplit);
+			//Resets the file descriptor and resets the standard output if we file redirected
+			if(ifRedirected == 1)
+			{	
+				close(file_desc);
+				dup2(terminal,1);
+				ifRedirected = 0;
 			}
-			else //parent
-			{
-				//wait for miscPID, error checking
-				if(waitpid(miscPID, NULL, 0) < 0)
-				{
-					printf("Command error\n"); 
-					free(currentDirectory); 
-					free(buf); 
-					free(TEMPLATE_DIR);
-					exit(0);
-				}
-			}
-		}
-
-		if(ifRedirected == 1)
-		{	
-			close(file_desc);
-			dup2(terminal,1);
-			ifRedirected = 0;
-		}
 		
 		}
-		else {
+		
+		//NEEDS CHECKING
+		else 
+		{
 			isPiping = 0;
 		}
 
@@ -650,8 +687,11 @@ int main(){
 			free(TEMPLATE_DIR);
 			exit(0);
 		}
-
+		
+		//Prints the shell tag and the current path
 		printf("%s%s $ ", SHELL_TAG, currentDirectory); 
+
+		//Waits and gets our next input
 		fgets(buf, MAX_COMMAND_SIZE, stdin);
 	}
 
